f9e85387dc2f1753e9c159461ada665626abdf67
Djokey <67658015+Djokey@users.noreply.github.com>
2020-07-04 00:18:05 +0700

    update

--- app/src/main/java/com/example/ygolyok/MainActivity.kt
+++ app/src/main/java/com/example/ygolyok/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.ygolyok
 
-import android.app.Dialog
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AlertDialog
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.DialogFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Заполняем спинер
+        val option : Spinner = findViewById(R.id.post_spinner)
+        val Post: Array<String> = arrayOf("Выберите поставщика","Аршановский","Кирбинский","Черногорский","Изыхский")
+        option.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Post)
+        //Заполнили первый спинер
+
     }
+
 
 
 
